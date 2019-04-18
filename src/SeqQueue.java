3eb910535d117cedc662c085c9d28ca6d3cd2aed
ZZMilkTEA <504267031@qq.com>
2019-04-18 16:37:37 +0800

    Added comments to the SeqQueue. Let the Yanghui triangle output in the test section become the number of manual input lines.

--- src/SeqQueue.java
+++ src/SeqQueue.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class SeqQueue {
     private int element[];
-    private int front;
-    private int rear;
-    private int mod;
+    private int front;  //队头
+    private int rear;   //队尾
+    private int mod;    //队列的模
 
     //构造函数
     SeqQueue(int maxSize){
@@ -55,6 +57,7 @@ public class SeqQueue {
         return e;
     }
 
+    //访问第n个元素
     public int visit(int no){
         return element[(front + no - 1) % mod];
     }
@@ -66,23 +69,30 @@ public class SeqQueue {
 
     //输出杨辉三角形的测试程序
     public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
         SeqQueue row = new SeqQueue(100);
 
         row.enterQueue(1);
 
-        int rowsCount = 20;
+        System.out.print("请输入输出行数：");
+        int rowsCount = scanner.nextInt();
 
-        for (int i = 2; i <= rowsCount;i++){
-            for (int j = 0; j < rowsCount - i; j++){
+        for (int i = 2; i <= rowsCount;i++){    //循环输出n-1次（除开最后一行的输出），其中i赋初值2是为了方便计算生成中间内容的循环次数
+            System.out.print("  ");
+            for (int j = 0; j < rowsCount - i; j++){ //对应生成n行前的空格数量，保持金字塔状
                 System.out.print("  ");
             }
             row.enterQueue(1);
-            for (int j = 0;j < i - 2; j++){
+            for (int j = 0;j < i - 2; j++){     //生成中间元素
                 row.enterQueue(row.visit(1) + row.visit(2));
                 System.out.print(row.deleteQueue() + "   ");
             }
-            System.out.println(row.deleteQueue());
-            row.enterQueue(1);
+            System.out.println(row.deleteQueue());  //n-1行最后一个元素出队并输出
+            row.enterQueue(1);  //第n行的第一个元素1入队
+        }
+
+        while (!row.isEmpty()){     //输出最后一行
+            System.out.print(row.deleteQueue() + "   ");
         }
 
     }
